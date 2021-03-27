c0c26caeababeb20262aa2b6b5ba30234c61f765
Miguel Silva <miguelcfsilva@gmail.com>
2021-03-27 16:03:35 +0000

    Filtering Version

--- Qualification 2021/Moons_umbrellas.py
+++ Qualification 2021/Moons_umbrellas.py
@@ -1,5 +1,6 @@
-def cost(mural, x, y):
+def cost(alist, x, y):
     price = 0
+    mural = "".join(alist)
     for letter in range(1, len(mural)):
         if mural[letter-1: letter+1] == "CJ":
             price += x
@@ -7,34 +8,12 @@ def cost(mural, x, y):
             price += y
     return price
 
-
-def generate_pick(mural, x, y):
-
-    aux = set()
-    altered = False
-    
-    for state in mural:
-        for letter in range(len(state)):
-            if state[letter] == "?":
-                altered = True
-                temp = list(state)
-                temp[letter] = "C"
-                aux.add("".join(temp))
-                temp[letter] = "J"
-                aux.add("".join(temp))
-
-    if altered:
-        return generate_pick(aux, x, y)  
-    return min([cost(state,x,y) for state in mural])
-
-
 t = int(input())
 case = 1
 while t > 0:
     alist = list(input().split(" "))
-    mural = set()
-    mural.add(alist[2])
     x, y = int(alist[0]), int(alist[1])
-    print(("Case #{0}: {1}").format(case, generate_pick(mural, x, y)))
+    mural = [letter for letter in list(alist[2]) if letter != "?"]
+    print(("Case #{0}: {1}").format(case, cost(mural, x, y)))
     case += 1
     t -= 1
