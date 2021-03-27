b302c4de9ba996e33cfb95d0ddae2729b8994c89
Miguel Silva <miguelcfsilva@gmail.com>
2021-03-27 15:57:34 +0000

    Updated Recursive Version

--- Qualification 2021/Moons_umbrellas.py
+++ Qualification 2021/Moons_umbrellas.py
@@ -1,41 +1,40 @@
 def cost(mural, x, y):
-
-    if "?" not in mural:
-        price = 0
-        for letter in range(1, len(mural)):
-            if mural[letter-1: letter+1] == "CJ":
-                price += x
-            elif mural[letter-1: letter+1] == "JC":
-                price += y
-        return price
-
-    threshold = 1*10**6
-
-    for pick in generate_pick(mural):
-        future_price = cost(pick, x, y)
-        if future_price < threshold:
-            threshold = future_price
-
-    return threshold
+    price = 0
+    for letter in range(1, len(mural)):
+        if mural[letter-1: letter+1] == "CJ":
+            price += x
+        elif mural[letter-1: letter+1] == "JC":
+            price += y
+    return price
 
 
-def generate_pick(mural):
-    possible_picks = []
-    for letter in range(len(mural)):
-        if mural[letter] == "?":
-            temp = list(mural)
-            temp[letter] = "C"
-            possible_picks.append("".join(temp))
-            temp[letter] = "J"
-            possible_picks.append("".join(temp))
-    return possible_picks
+def generate_pick(mural, x, y):
+
+    aux = set()
+    altered = False
+    
+    for state in mural:
+        for letter in range(len(state)):
+            if state[letter] == "?":
+                altered = True
+                temp = list(state)
+                temp[letter] = "C"
+                aux.add("".join(temp))
+                temp[letter] = "J"
+                aux.add("".join(temp))
+
+    if altered:
+        return generate_pick(aux, x, y)  
+    return min([cost(state,x,y) for state in mural])
 
 
 t = int(input())
 case = 1
 while t > 0:
     alist = list(input().split(" "))
-    x, y, mural = int(alist[0]), int(alist[1]), alist[2]
-    print(("Case #{0}: {1}").format(case, cost(mural, x, y)))
+    mural = set()
+    mural.add(alist[2])
+    x, y = int(alist[0]), int(alist[1])
+    print(("Case #{0}: {1}").format(case, generate_pick(mural, x, y)))
     case += 1
     t -= 1
